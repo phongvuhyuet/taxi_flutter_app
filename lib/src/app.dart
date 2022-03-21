8584ba10c8ac8db3066e47f14a894d2851da3edb
phongltktv <vuphonghyuet@gmail.com>
2022-03-21 22:05:58 +0700

    feat(RegisterPage): basic UI

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:taxi_flutter_app/resources/login_page.dart';
+import 'package:taxi_flutter_app/resources/register_page.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: LoginPage());
+    return MaterialApp(home: RegisterPage());
   }
 }
